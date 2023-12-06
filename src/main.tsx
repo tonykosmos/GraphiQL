ace2ad82699f30f3d244b6f7cc9da594f7f7bd70
oleg-gobrik <oleg.gobrik@gmail.com>
2023-12-06 16:25:02 +0300

    feat: add error-boundary to the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import ErrorBoundary from './components/error-boundary';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>GraphiQL</h1>
+    <ErrorBoundary>
+      <h1>GraphiQL</h1>
+    </ErrorBoundary>
   </React.StrictMode>
 );
